48226ddc8ab0f59c0405f7bce39e12d77f39bd2c
DESKTOP-8RGBQ8K\ryuyong <dolly11@naver.com>
2017-06-17 17:40:42 +0900

    committest2

--- src/com/word/wordbookmake/LogUtil.java
+++ src/com/word/wordbookmake/LogUtil.java
@@ -4,13 +4,13 @@ import android.util.Log;
 
 public class LogUtil {
 	private static boolean isDevMode = true;
-
+	//디버그용 
 	public static void d(String tag, String msg) {
 		if (isDevMode) {
 			Log.d(tag, msg);
 		}
 	}
-	
+	//운영용
 	public static void i(String tag, String msg) {
 		if (isDevMode) {
 			Log.i(tag, msg);
